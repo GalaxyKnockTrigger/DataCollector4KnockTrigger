ae2fc9bd2e2759270a79a94864e82e1ddea3df52
girinssh <tngus5851@gmail.com>
2022-03-25 02:24:31 +0900

    Detach TrustOkHttpClientUtil from MainActivity

--- app/src/main/java/com/capstone/knockdatareceiver4watch/MainActivity.java
+++ app/src/main/java/com/capstone/knockdatareceiver4watch/MainActivity.java
@@ -122,36 +122,6 @@ public class MainActivity extends Activity {
         recorder = null;
     }
 
-    static class TrustOkHttpClientUtil {
-        public static OkHttpClient.Builder getUnsafeOkHttpClient() {
-            try { // Create a trust manager that does not validate certificate chains
-                final TrustManager[] trustAllCerts = new TrustManager[]{
-                        new X509TrustManager() {
-                            @Override
-                            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-
-                            }
-                            @Override public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException { }
-                            @Override public java.security.cert.X509Certificate[] getAcceptedIssuers() { return new java.security.cert.X509Certificate[]{};
-                            }
-                        }
-                }; // Install the all-trusting trust manager
-                final SSLContext sslContext = SSLContext.getInstance("SSL");
-                sslContext.init(null, trustAllCerts, new java.security.SecureRandom()); // Create an ssl socket factory with our all-trusting manager
-                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-                OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
-                builder.hostnameVerifier(new HostnameVerifier() {
-                    @Override public boolean verify(String hostname, SSLSession session) {
-                        return true;
-                    }
-                });
-                return builder;
-            } catch (Exception e) { throw new RuntimeException(e);
-            }
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -241,8 +211,8 @@ public class MainActivity extends Activity {
 
         binding.setTime("");
         binding.setActivity(this);
-
-        myContext = this.getApplicationContext();
+//
+//        myContext = this.getApplicationContext();
 
     }
 
@@ -340,11 +310,11 @@ public class MainActivity extends Activity {
         }
     }
 
-    private static Context myContext = null;
-
-    public static Context getAppContext(){
-        return myContext;
-    }
+//    private static Context myContext = null;
+//
+//    public static Context getAppContext(){
+//        return myContext;
+//    }
 }
 //
 //class SelfSigningHelper {
