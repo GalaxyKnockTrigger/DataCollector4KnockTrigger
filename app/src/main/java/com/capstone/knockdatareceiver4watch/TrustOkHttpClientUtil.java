8f5cf2708adb4021f85441fcfc19227d1a269df5
girinssh <tngus5851@gmail.com>
2022-04-15 07:00:48 +0900

    Edit comments

--- app/src/main/java/com/capstone/knockdatareceiver4watch/TrustOkHttpClientUtil.java
+++ app/src/main/java/com/capstone/knockdatareceiver4watch/TrustOkHttpClientUtil.java
@@ -1,6 +1,11 @@
 package com.capstone.knockdatareceiver4watch;
 
+import android.util.Log;
+
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
@@ -9,11 +14,15 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import okhttp3.CipherSuite;
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 
 public class TrustOkHttpClientUtil {
     public static OkHttpClient.Builder getUnsafeOkHttpClient() {
-        try { // Create a trust manager that does not validate certificate chains
+        try {
+            // Create a trust manager that does not validate certificate chains
             final TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
                         @Override
@@ -24,19 +33,33 @@ public class TrustOkHttpClientUtil {
                         @Override public java.security.cert.X509Certificate[] getAcceptedIssuers() { return new java.security.cert.X509Certificate[]{};
                         }
                     }
-            }; // Install the all-trusting trust manager
+            };
+            // Install the all-trusting trust manager
             final SSLContext sslContext = SSLContext.getInstance("SSL");
             sslContext.init(null, trustAllCerts, new java.security.SecureRandom()); // Create an ssl socket factory with our all-trusting manager
             final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+            ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                    .tlsVersions(TlsVersion.TLS_1_3)
+                    .cipherSuites(
+                            CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                            CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                    .build();
+            OkHttpClient.Builder builder = new OkHttpClient.Builder().connectionSpecs(Arrays.asList(new ConnectionSpec[]{spec}));
             builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
             builder.hostnameVerifier(new HostnameVerifier() {
                 @Override public boolean verify(String hostname, SSLSession session) {
+                    Log.i("TrustOHCU", "PeerHost: " + session.getPeerHost());
+                    Log.i("TrustOHCU", "PeerPort: " + session.getPeerPort());
+                    Log.i("TrustOHCU", "HostName: " + hostname);
                     return true;
                 }
             });
             return builder;
-        } catch (Exception e) { throw new RuntimeException(e);
+        } catch (Exception e) {
+            Log.i("TrustOHCU", "Failed");
+            throw new RuntimeException(e);
         }
     }
 }
