2c2eca88afc238ca1a7d52a3e811722f89ca7dd0
girinssh <tngus5851@gmail.com>
2022-04-28 04:07:04 +0900

    Add Constant Threshold about sound peak

--- app/src/main/java/com/capstone/knockdatareceiver4watch/Constants.java
+++ app/src/main/java/com/capstone/knockdatareceiver4watch/Constants.java
@@ -8,7 +8,10 @@ public class Constants {
     public static final int TYPE_GYRO = Sensor.TYPE_GYROSCOPE;
     public static final String[] permissions = {Manifest.permission.RECORD_AUDIO};
     public static final int RCODE = 10000;
-    public static final int RECEIVING_TIME = 1000;
+    public static final int RECEIVING_TIME = 2000;
     public static final String URL = "https://nas.splo2t.com";
     public static final int PORT = 9999;
+
+
+    public static final short TH_SOUND_PEAK = 5000;
 }
